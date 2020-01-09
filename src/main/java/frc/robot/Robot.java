486e258e60c27a18759d180c60b4c8420d4e9107
Joe S <31870999+KenwoodFox@users.noreply.github.com>
2020-01-09 11:04:11 -0500

    Delete unused

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -49,11 +49,7 @@ public class Robot extends TimedRobot {
    */
   @Override
   public void robotPeriodic() {
-    // Runs the Scheduler.  This is responsible for polling buttons, adding newly-scheduled
-    // commands, running already-scheduled commands, removing finished or interrupted commands,
-    // and running subsystem periodic() methods.  This must be called from the robot's periodic
-    // block in order for anything in the Command-based framework to work.
-    CommandScheduler.getInstance().run();
+    CommandScheduler.getInstance().run(); // Runs the entire Scheduler, not to be messed with.
   }
 
   /**
@@ -61,22 +57,23 @@ public class Robot extends TimedRobot {
    */
   @Override
   public void disabledInit() {
+    arcadeDrive.cancel(); // Stop the arcade drive command.
   }
 
+  /** This function is called all the time during Disabled mode */
   @Override
   public void disabledPeriodic() {
   }
 
   /**
-   * This autonomous runs the autonomous command selected by your {@link RobotContainer} class.
+   * This function runs once each time the robot enters autonomous mode
    */
   @Override
   public void autonomousInit() {
-    m_autonomousCommand = m_robotContainer.getAutonomousCommand();
+    m_autonomousCommand = m_robotContainer.getAutonomousCommand(); // Get the selected autonomous command
 
-    // schedule the autonomous command (example)
-    if (m_autonomousCommand != null) {
-      m_autonomousCommand.schedule();
+    if (m_autonomousCommand != null) { // If the command is not null
+      m_autonomousCommand.schedule(); // Run the command
     }
   }
 
@@ -89,15 +86,11 @@ public class Robot extends TimedRobot {
 
   @Override
   public void teleopInit() {
-    // This makes sure that the autonomous stops running when
-    // teleop starts running. If you want the autonomous to
-    // continue until interrupted by another command, remove
-    // this line or comment it out.
-    if (m_autonomousCommand != null) {
-      m_autonomousCommand.cancel();
+    if (m_autonomousCommand != null) { // If the command that was run at autonomous init was not null
+      m_autonomousCommand.cancel(); // Cancel the autonomous command
     }
 
-    arcadeDrive.schedule();
+    arcadeDrive.schedule(); // Run the arcade drive command
   }
 
   /**
