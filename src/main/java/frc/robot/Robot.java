b47f32ed8690e83f08e9e8db4cf8f4adc4e3800c
Joe S <31870999+KenwoodFox@users.noreply.github.com>
2020-01-13 09:22:32 -0500

    Change subsystem instance from subsystem to drivetrain

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -12,6 +12,7 @@ import edu.wpi.cscore.UsbCamera;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import frc.robot.commands.HumanControl;
 
 /**
  * The VM is configured to automatically run this class, and to call the functions corresponding to
--- src/main/java/frc/robot/commands/HumanControl.java
+++ src/main/java/frc/robot/commands/HumanControl.java
@@ -9,14 +9,14 @@ import frc.robot.subsystems.Drivetrain;
 
 public class HumanControl extends CommandBase {
     
-  private final Drivetrain subsystem;
+  private final Drivetrain drivetrain;
   private final DoubleSupplier thro;
   private final DoubleSupplier steerage;
   private final Supplier<HandlingMode> handlingMode;
 
   public HumanControl(DoubleSupplier _thro, DoubleSupplier _steerage, Supplier<HandlingMode> _drivingMode, Drivetrain _drivetrain) {
     addRequirements(_drivetrain);
-    subsystem = _drivetrain;
+    drivetrain = _drivetrain;
     thro = _thro;
     steerage = _steerage;
     handlingMode = _drivingMode;
@@ -26,7 +26,7 @@ public class HumanControl extends CommandBase {
   public void execute() {
     switch(handlingMode.get()){
       case kStandard:
-        subsystem.FlyByWireA(thro.getAsDouble(), steerage.getAsDouble());
+        drivetrain.FlyByWireA(thro.getAsDouble(), steerage.getAsDouble());
       case kDiffLock:
       case kLaneAssist:
       case kFlyByWire:
