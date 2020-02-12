b45c1c4ec285988e8b5560b5b02da28957bafcf8
Joe S <31870999+KenwoodFox@users.noreply.github.com>
2020-02-12 13:06:00 -0500

    Finish up test shooter command

--- src/main/java/frc/robot/commands/TestShooter.java
+++ src/main/java/frc/robot/commands/TestShooter.java
@@ -26,24 +26,9 @@ public class TestShooter extends CommandBase {
     test_speed = _test_speed;
   }
 
-  // Called when the command is initially scheduled.
-  @Override
-  public void initialize() {
-  }
-
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-  }
-
-  // Called once the command ends or is interrupted.
-  @Override
-  public void end(boolean interrupted) {
-  }
-
-  // Returns true when the command should end.
-  @Override
-  public boolean isFinished() {
-    return false;
+    shooter.testShooter(test_speed.getAsDouble());
   }
 }
