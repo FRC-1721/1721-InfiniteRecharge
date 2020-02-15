dc29c94559de67e91946ad4a79c2926bf593a647
Joe S <31870999+KenwoodFox@users.noreply.github.com>
2020-02-15 01:14:43 -0500

    Clean up imports

--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -19,7 +19,6 @@ import edu.wpi.first.wpilibj.Solenoid;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
-import frc.robot.Constants.TurretPID;
 
 public class Shooter extends SubsystemBase {
   // TalonSRX objects
