829ae94b36753a42dd1be094fb632df467c48ac1
Joe S <31870999+KenwoodFox@users.noreply.github.com>
2020-02-18 19:18:24 -0500

    Remove unused imports

--- src/main/java/frc/robot/commands/ManualShooter.java
+++ src/main/java/frc/robot/commands/ManualShooter.java
@@ -10,7 +10,6 @@ package frc.robot.commands;
 import java.util.function.DoubleSupplier;
 
 import edu.wpi.first.wpilibj.GenericHID.RumbleType;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.Shooter;
