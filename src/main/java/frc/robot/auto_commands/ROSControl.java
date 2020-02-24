49cb1cf8737e84ea402c0775868ce61ff71f002f
Joe S <31870999+KenwoodFox@users.noreply.github.com>
2020-02-24 12:21:32 -0500

    Move ROS commands into new package: ros_commands

--- src/main/java/frc/robot/auto_commands/ROSControl.java
+++ src/main/java/frc/robot/auto_commands/ROSControl.java
@@ -5,7 +5,7 @@
 /* the project.                                                               */
 /*----------------------------------------------------------------------------*/
 
-package frc.robot.commands;
+package frc.robot.auto_commands;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.Drivetrain;
