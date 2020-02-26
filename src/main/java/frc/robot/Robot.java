50bbe69eb3f0d25576a1c8e39723903de80f2be5
Joe S <31870999+KenwoodFox@users.noreply.github.com>
2020-02-26 18:23:40 -0500

    Add startup alert to robot.java

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -8,6 +8,7 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 
@@ -28,6 +29,8 @@ public class Robot extends TimedRobot {
    */
   @Override
   public void robotInit() {
+    SmartDashboard.putString("Alert", "Robot Program Started");
+    
     m_robotContainer = new RobotContainer(); // Define our Robot Container
   }
 
