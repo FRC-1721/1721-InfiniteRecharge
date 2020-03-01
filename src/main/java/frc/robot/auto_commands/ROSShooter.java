ebb6b51ad99e8eb09b29b368ab2fa486aa07c863
Joe S <31870999+KenwoodFox@users.noreply.github.com>
2020-03-01 12:42:32 -0500

    Clean merge

--- src/main/java/frc/robot/auto_commands/ROSShooter.java
+++ src/main/java/frc/robot/auto_commands/ROSShooter.java
@@ -8,7 +8,7 @@
 package frc.robot.auto_commands;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
-import frc.robot.subsystems.ROS;
+import frc.robot.ros.ROS;
 import frc.robot.subsystems.Shooter;
 import frc.robot.subsystems.Turret;
 
--- src/main/java/frc/robot/ros/ROS.java
+++ src/main/java/frc/robot/ros/ROS.java
@@ -20,6 +20,7 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 import frc.robot.subsystems.Drivetrain;
+import frc.robot.subsystems.Turret;
 
 public class ROS extends SubsystemBase {
   //From SmartDashboard,class
