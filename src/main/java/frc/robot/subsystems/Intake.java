541eba6bcb387758a3371daffe38c6deaa9cab2e
KenwoodFox <kenwood364@gmail.com>
2021-04-02 14:29:41 -0400

    This change just to test if checkstyle fails.

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -13,6 +13,9 @@ import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
+
+
+
 public class Intake extends SubsystemBase {
   // Motor objects
   private CANSparkMax intakeMotor;
